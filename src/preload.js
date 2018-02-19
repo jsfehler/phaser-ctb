808263a1f2a545fa1bfdfde61c26442f084265d5
Joshua Fehler <jsfehler@users.noreply.github.com>
2018-02-18 20:58:19 -0500

    Fix tab/space confusion in preload.js

--- src/preload.js
+++ src/preload.js
@@ -3,19 +3,19 @@ var preload = function (game) {
 };
 
 preload.prototype = {
-	preload: function () {
+    preload: function () {
         "use strict";
         var loadingBar = this.add.sprite(160, 240, "loading");
         loadingBar.anchor.setTo(0.5, 0.5);
         this.load.setPreloadSprite(loadingBar);
 
-		// UI
+	// UI
         this.game.load.image("p1_icon", "assets/p1_icon.png");
         this.game.load.image("p2_icon", "assets/p2_icon.png");
         this.game.load.image("m1_icon", "assets/m1_icon.png");
-        
+
         this.game.load.image("waitButton", "assets/wait_button.png");
-         
+
     },
     create: function () {
         "use strict";
