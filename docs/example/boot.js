62fc8b4a2d3633f5e7b8a60d0f8d78d24094c0df
Joshua Fehler <jsfehler@gmail.com>
2018-02-25 13:28:00 -0500

    Move demo files into separate directory

--- docs/example/boot.js
+++ docs/example/boot.js
@@ -6,10 +6,10 @@ boot.prototype = {
     preload: function () {
         "use strict";
         this.game.load.image("loading", "assets/loading.png");
-	},
+    },
     create: function () {
 	"use strict";
         this.game.stage.backgroundColor = "#252525";
-	this.game.state.start("Preload");
-	}
+        this.game.state.start("Preload");
+    }
 };
--- docs/example/preload.js
+++ docs/example/preload.js
@@ -9,7 +9,7 @@ preload.prototype = {
         loadingBar.anchor.setTo(0.5, 0.5);
         this.load.setPreloadSprite(loadingBar);
 
-	// UI
+        // UI
         this.game.load.image("p1_icon", "assets/p1_icon.png");
         this.game.load.image("p2_icon", "assets/p2_icon.png");
         this.game.load.image("m1_icon", "assets/m1_icon.png");
